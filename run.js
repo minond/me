04b3696ea9dc5a7ae175808b05dfb019f0fd2b31
Marcos Minond <minond.marcos@gmail.com>
2014-06-15 17:16:41 -0600

    summarizing github data

--- run.js
+++ run.js
@@ -2,20 +2,42 @@
 
 var Github = require('./src/sources/github');
 
-var since = new Date('2014-05-01 00:00:00'),
-    until = new Date('2014-06-14 23:59:00');
+var now = new Date(),
+    since = new Date(now.setHours(0, 0, 0)),
+    until = new Date(now.setHours(24, 0, 0));
 
 var github = new Github(
     process.env.GITHUB_OAUTH_USER,
     process.env.GITHUB_OAUTH_TOKEN
 );
 
+console.log('getting commits from %s to %s', since, until);
 github.repos().then(function (repos) {
-    console.log(repos);
+    var data = {};
+
+    repos.forEach(function (repo) {
+        data[ repo.id ] = {
+            name: repo.name,
+            full_name: repo.full_name,
+            url: repo.html_url
+        };
+    });
+
+    console.log(data);
 
     repos.forEach(function (repo) {
         github.commits(repo, since, until).then(function (commits) {
-            console.log(commits);
+            var data = [];
+
+            commits.forEach(function (commit) {
+                data.push({
+                    sha: commit.sha,
+                    url: commit.html_url,
+                    repo: repo.id
+                });
+            });
+
+            console.log(data);
         });
     });
 });
