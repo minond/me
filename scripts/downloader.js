20643c0aa3d71088989fb8189437536107c1a2b6
Marcos Minond <minond.marcos@gmail.com>
2014-06-22 15:34:01 -0600

    checking mongo connection once per hour

--- scripts/downloader.js
+++ scripts/downloader.js
@@ -3,13 +3,32 @@
 var every = require('../src/every'),
     mongojs = require('mongojs'),
     me = mongojs('me', ['data']),
-    storage = require('../src/upserts')(me.data);
+    storage = require('../src/upserts')(me.data),
+    log = require('debug')('worker');
 
 var Github = require('../src/sources/github'),
     weather = require('weather-js');
 
 var get_github_data = require('../src/getters/github'),
     get_weather_data = require('../src/getters/weather');
+
+// check mongo connection
+every.hour(function () {
+    log('checking mongo stats');
+    me.stats(function (err, stats) {
+        if (err) {
+            log('error while checking mongo stats: %s', err.message);
+            log('ending worker process');
+            process.exit(1);
+        } else if (!stats.ok) {
+            log('mongo connection failure');
+            log('ending worker process');
+            process.exit(1);
+        } else {
+            log('mongo connection up and running');
+        }
+    });
+});
 
 // github data
 every(12).hours(function () {
