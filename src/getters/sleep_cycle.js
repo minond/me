5921aa63045e1f09a2c1527f4a3f832c6df09fbb
Marcos Minond <minond.marcos@gmail.com>
2014-07-05 14:36:44 -0600

    saving sleep quality in sleep_cycle getter

--- src/getters/sleep_cycle.js
+++ src/getters/sleep_cycle.js
@@ -13,7 +13,7 @@ module.exports = function get_sleep_cycle_data (storage, sleep_data) {
         // {
         //     start: '2014-03-14 00:43:59',
         //     end: '2014-03-14 07:24:42',
-        //     Sleep_quality: '73%',
+        //     sleep_quality: '73%',
         //     time_in_bed: '6:40',
         //     wake_up: '',
         //     sleep_notes: 'Drank coffee:Drank tea:Stressful day',
@@ -21,16 +21,26 @@ module.exports = function get_sleep_cycle_data (storage, sleep_data) {
         //     activity_steps: '0'
         // }
         rows.forEach(function (row) {
-            var entry, start, end, suid;
+            var entry, start, end, quality, time, suid;
 
             start = new Date(row.start);
             end = new Date(row.end);
             suid = [ +start, +end ].join('');
 
+            // quality % to time
+            quality = parseInt(row.sleep_quality);
+
+            // time hh:mm to seconds
+            time = row.time_in_bed.split(':');
+            time = (parseInt(time[0]) * 60 * 60) +
+                (parseInt(time[1]) * 60);
+
             entry = new Entry('sleep', suid, {
                 start: start,
                 end: end,
-                time: row.time_in_bed,
+                time_human: row.time_in_bed,
+                time: time,
+                quality: quality,
                 notes: row.sleep_notes
                     .toLowerCase()
                     .split(':'),
