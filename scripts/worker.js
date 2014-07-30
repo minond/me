d3e77c158d54c945c4602a7125ca607807268b2f
Marcos Minond <minond.marcos@gmail.com>
2014-07-29 21:10:45 -0600

    adding counter task to worker

--- scripts/worker.js
+++ scripts/worker.js
@@ -1,11 +1,14 @@
 'use strict';
 
+var Entry = require('../src/entry');
+
 var every = require('../src/every'),
     tasks = require('../src/tasks/index'),
+    lodash = require('lodash'),
     log = require('debug')('worker');
 
+// check we're still connected to mongo. kill process on error
 every.hour(function () {
-    // check we're still connected to mongo. kill process on error
     log('checking mongo stats');
     tasks.database.stats(function (err, stats) {
         if (err) {
@@ -19,6 +22,19 @@ every.hour(function () {
         } else {
             log('mongo connection up and running');
         }
+    });
+});
+
+every(5).minutes(function () {
+    log('fetching entry counts');
+    lodash.each(Entry.schema.labels, function (type, label) {
+        tasks.storage.count({ label: label, type: type }, function (err, count) {
+            if (err) {
+                log('error getting count for %s (%s)', label, type);
+            } else {
+                log('%s (%s): %s', label, type, count);
+            }
+        });
     });
 });
 
