5b69678ae675a1835bbb2c8caf333f6e1b012f98
Marcos Minond <minond.marcos@gmail.com>
2015-01-07 20:03:52 -0700

    code cleanup

--- src/getters/action/commits/github.js
+++ src/getters/action/commits/github.js
@@ -9,6 +9,82 @@ var point = require('../../../point'),
     log = require('debug')('getter:action:commits:github');
 
 /**
+ * takes a commit object and returns a point to be saved
+ * @param {Object} commit object we got back from Github.commit
+ * @return {Point}
+ */
+function extract_commit_info(commit) {
+    var date, entry;
+
+    date = new Date(commit.commit.committer.date);
+    entry = point(TYPE, SUBTYPE, SOURCE, date, commit.sha, {
+        sha: commit.sha,
+        url: commit.html_url,
+        message: commit.commit.message,
+        stats: commit.stats || {},
+        files: []
+    });
+
+    if (commit.files) {
+        commit.files.forEach(function (file) {
+            entry.data.files.push({
+                name: file.filename,
+                type: mime.lookup(file.filename)
+            });
+        });
+    }
+
+    return entry;
+}
+
+/**
+ * download commit info and saves it
+ * @param {Github} github
+ * @param {Object} storage
+ * @param {Object} repo object sent back from Github.repos
+ */
+function save_commit_info(github, storage, repo) {
+    return function (commit) {
+        github.commit(repo.name, commit.sha).then(function (commit) {
+            var entry = extract_commit_info(commit);
+
+            log('saving %o', entry);
+            storage.push(entry);
+        });
+    };
+}
+
+/**
+ * downloads commits for a repo within a date range
+ * @param {Github} github
+ * @param {Object} storage
+ * @param {Date} since
+ * @param {Date} until
+ */
+function get_commits_for_repo(github, storage, since, until) {
+    return function (repo) {
+        log('downloading commits for %s', repo.name);
+
+        (function getcommits(page) {
+            github.commits(repo.name, since, until, page).then(function (commits) {
+                if (commits.length) {
+                    getcommits(page + 1);
+                } else if (page === 1) {
+                    log('nothing commited in %s', repo.name);
+                }
+
+                if (!commits.length) {
+                    return;
+                }
+
+                log('saving %s commits for %s', commits.length, repo.name);
+                commits.forEach(save_commit_info(github, storage, repo));
+            });
+        })(1);
+    };
+}
+
+/**
  * gets all commits from all repos from github
  * @param {Github} github
  * @param {Object} storage
@@ -22,49 +98,6 @@ module.exports = function (github, storage, range) {
     log('getting commits from %s to %s', since, until);
     github.repos().then(function (repos) {
         log('starting to download commits for %s repos', repos.length);
-
-        repos.forEach(function (repo) {
-            log('downloading commits for %s', repo.name);
-
-            (function getcommits(page) {
-                github.commits(repo.name, since, until, page).then(function (commits) {
-                    if (commits.length) {
-                        getcommits(page + 1);
-                    } else if (page === 1) {
-                        log('nothing commited in %s', repo.name);
-                    }
-
-                    if (!commits.length) {
-                        return;
-                    }
-
-                    log('saving %s commits for %s', commits.length, repo.name);
-                    commits.forEach(function (commit) {
-                        github.commit(repo.name, commit.sha).then(function (commit) {
-                            var date = new Date(commit.commit.committer.date),
-                                entry = point(TYPE, SUBTYPE, SOURCE, date, commit.sha, {
-                                sha: commit.sha,
-                                url: commit.html_url,
-                                message: commit.commit.message,
-                                stats: commit.stats || {},
-                                files: []
-                            });
-
-                            if (commit.files) {
-                                commit.files.forEach(function (file) {
-                                    entry.data.files.push({
-                                        name: file.filename,
-                                        type: mime.lookup(file.filename)
-                                    });
-                                });
-                            }
-
-                            log('saving %o', entry);
-                            storage.push(entry);
-                        });
-                    });
-                });
-            })(1);
-        });
+        repos.forEach(get_commits_for_repo(github, storage, since, until));
     });
 };
--- src/getters/environment/weather/forecast_io.js
+++ src/getters/environment/weather/forecast_io.js
@@ -8,6 +8,28 @@ var point = require('../../../point'),
     log = require('debug')('getter:environment:weather:forecast_io');
 
 /**
+ * takes a forecast object and returns a point to be saved
+ * @param {Object} forecast object we got back from ForecastIo.forecast
+ * @param {moment.date} day
+ * @return {Point}
+ */
+function extract_forecast_info(forecast, day) {
+    var data = forecast.daily.data.shift();
+    return point(TYPE, SUBTYPE, SOURCE, day.toDate(), day.unix(), {
+        summary: data.summary,
+        icon: data.icon,
+        min: data.temperatureMin,
+        max: data.temperatureMax,
+        wind: data.windSpeed,
+        humidity: data.humidity,
+        visibility: data.visibility,
+        clouds: data.cloudCover,
+        pressure: data.pressure,
+        ozone: data.ozone
+    });
+}
+
+/**
  * gets a daily summary of the weather from forecast.io
  * @param {ForecastIo} forecast_io
  * @param {Object} storage
@@ -22,19 +44,7 @@ module.exports = function (forecast_io, storage, range, config) {
         log('getting weather (%s, %s) data for %s', lon, lat, day);
 
         forecast_io.forecast(lat, lon, day.unix()).then(function (forecast) {
-            var data = forecast.daily.data.shift(),
-                entry = point(TYPE, SUBTYPE, SOURCE, day.toDate(), day.unix(), {
-                    summary: data.summary,
-                    icon: data.icon,
-                    min: data.temperatureMin,
-                    max: data.temperatureMax,
-                    wind: data.windSpeed,
-                    humidity: data.humidity,
-                    visibility: data.visibility,
-                    clouds: data.cloudCover,
-                    pressure: data.pressure,
-                    ozone: data.ozone
-                });
+            var entry = extract_forecast_info(forecast, day);
 
             log('saving %o', entry);
             storage.push(entry);
